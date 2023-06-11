0ba4b21885025a1e2c24d0205395fc71e933e30d
chadmuro <chadmuro@gmail.com>
2023-06-11 23:42:30 +0900

    feat: Add base layout for top landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,91 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import './styles.css';
+</script>
+
+<main>
+	<div class="title-container">
+		<div class="title-wrapper">
+			<img src="/mainichi-logo.svg" alt="Mainichi logo" class="mainichi-img" />
+			<h1>Mainichi <span class="focus-text">Habits</span></h1>
+		</div>
+		<p>
+			Make your <span class="focus-text">habits</span> stick and build your
+			<span class="focus-text">streak</span>!
+		</p>
+
+		<a
+			class="app-store-button"
+			href="https://www.chadmuro.com"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			<img src="/app-store-black.svg" alt="Download on the app store" class="app-store-img" />
+		</a>
+	</div>
+	<div class="screenshot-container">
+		<p>Screenshot of app</p>
+	</div>
+</main>
+
+<footer>
+	<p>© Chad Murobayashi 2023</p>
+</footer>
+
+<style>
+	/* Laayout */
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		@media only screen and (min-width: 768px) {
+			flex-direction: row;
+		}
+	}
+	footer {
+		/* background-color: red; */
+		display: flex;
+		justify-content: center;
+	}
+
+	/* Title container */
+	.title-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+
+	.title-wrapper {
+		display: flex;
+		gap: 20px;
+	}
+	.focus-text {
+		color: var(--primary);
+	}
+
+	.mainichi-img {
+		width: 40px;
+		height: auto;
+	}
+
+	.app-store-button {
+		max-width: 300px;
+		width: 100%;
+		height: auto;
+		background-color: inherit;
+		border: none;
+		cursor: pointer;
+	}
+
+	.app-store-img {
+		max-width: 300px;
+		width: 100%;
+		height: 100%;
+	}
+
+	/* Screenshot container */
+	.screenshot-container {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+</style>
